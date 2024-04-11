152b7b91d613d9c33969f998f0e3a3c01ca64457
VodeniZeko <saletovic.developer@gmail.com>
2024-04-11 00:15:15 -0500

    fetch clothing data using async/await

--- server/routes/clothing.js
+++ server/routes/clothing.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const fsPromises = require("fs").promises;
 const datafile = "server/data/clothing.json";
 const router = express.Router();
 
@@ -7,7 +8,6 @@ const router = express.Router();
 
 //1. using callbacks
 
-// Define route handler for the root URL '/'
 // router.route("/").get(function (req, res) {
 //   // Asynchronously fetch clothing data
 //   getClothingData((err, data) => {
@@ -44,40 +44,56 @@ const router = express.Router();
 
 //2. using promises
 
-// Define route handler for the root URL '/'
-router.route("/").get((req, res) => {
-  // Call getClothingData function, which returns a Promise
-  getClothingData()
-    // If Promise is resolved (success), log the data
-    .then((data) => {
-      console.log("Returning clothing data");
-      res.send(data);
-    })
-    // If Promise is rejected (error), log the error
-    .catch((err) => console.log(err))
-    // Finally, log "done" regardless of Promise outcome
-    .finally(() => console.log("done"));
+// router.route("/").get((req, res) => {
+//   // Call getClothingData function, which returns a Promise
+//   getClothingData()
+//     // If Promise is resolved (success), log the data
+//     .then((data) => {
+//       console.log("Returning clothing data");
+//       res.send(data);
+//     })
+//     // If Promise is rejected (error), log the error
+//     .catch((err) => console.log(err))
+//     // Finally, log "done" regardless of Promise outcome
+//     .finally(() => console.log("done"));
 
-  // Code execution continues here while Promise is being resolved/rejected
-  console.log("Doing more work");
+//   // Code execution continues here while Promise is being resolved/rejected
+//   console.log("Doing more work");
+// });
+
+// // Function to fetch clothing data asynchronously and return a Promise
+// function getClothingData() {
+//   return new Promise((resolve, reject) => {
+//     // Read data from file asynchronously
+//     fs.readFile(datafile, "utf8", (err, data) => {
+//       // Callback function to handle the result of readFile
+//       if (err) {
+//         // If an error occurred during file reading, reject the Promise with the error
+//         reject(err);
+//       } else {
+//         // If file reading was successful, parse the data and resolve the Promise with the result
+//         let clothingData = JSON.parse(data);
+//         resolve(clothingData);
+//       }
+//     });
+//   });
+// }
+
+//3. using async/await
+
+router.route("/").get(async (req, res) => {
+  try {
+    let clothingData = await getClothingData();
+    res.send(clothingData);
+  } catch (error) {
+    console.log('error', error);
+  }
 });
 
-// Function to fetch clothing data asynchronously and return a Promise
-function getClothingData() {
-  return new Promise((resolve, reject) => {
-    // Read data from file asynchronously
-    fs.readFile(datafile, "utf8", (err, data) => {
-      // Callback function to handle the result of readFile
-      if (err) {
-        // If an error occurred during file reading, reject the Promise with the error
-        reject(err);
-      } else {
-        // If file reading was successful, parse the data and resolve the Promise with the result
-        let clothingData = JSON.parse(data);
-        resolve(clothingData);
-      }
-    });
-  });
+async function getClothingData() {
+  let rawData = await fsPromises.readFile(datafile, "utf8");
+  let clothingData = JSON.parse(rawData);
+  return clothingData;
 }
 
 
